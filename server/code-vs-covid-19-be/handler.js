b1d41d98961b92f4fb86469290a6210c7c2da8e0
Manuel Schlegel <manuel.schlegel@netlight.com>
2020-03-29 00:04:55 +0100

    Added new Serverless Setup with AWS Aurora Database.

---
+++ server/code-vs-covid-19-be/handler.js
@@ -0,0 +1,122 @@
+const connectToDatabase = require("./db"); // initialize connection
+
+// simple Error constructor for handling HTTP error codes
+function HTTPError(statusCode, message) {
+  const error = new Error(message);
+  error.statusCode = statusCode;
+  return error;
+}
+
+module.exports.healthCheck = async () => {
+  await connectToDatabase();
+  console.log("Connection successful.");
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Connection successful." })
+  };
+};
+
+module.exports.create = async event => {
+  try {
+    const { Note } = await connectToDatabase();
+    const note = await Note.create(JSON.parse(event.body));
+    return {
+      statusCode: 200,
+      body: JSON.stringify(note)
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { "Content-Type": "text/plain" },
+      body: "Could not create the note."
+    };
+  }
+};
+
+module.exports.getOne = async event => {
+  try {
+    const { Note } = await connectToDatabase();
+    const note = await Note.findById(event.pathParameters.id);
+    if (!note)
+      throw new HTTPError(
+        404,
+        `Note with id: ${event.pathParameters.id} was not found`
+      );
+    return {
+      statusCode: 200,
+      body: JSON.stringify(note)
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { "Content-Type": "text/plain" },
+      body: err.message || "Could not fetch the Note."
+    };
+  }
+};
+
+module.exports.getAll = async () => {
+  try {
+    const { Note } = await connectToDatabase();
+    const notes = await Note.findAll();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(notes)
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { "Content-Type": "text/plain" },
+      body: "Could not fetch the notes."
+    };
+  }
+};
+
+module.exports.update = async event => {
+  try {
+    const input = JSON.parse(event.body);
+    const { Note } = await connectToDatabase();
+    const note = await Note.findById(event.pathParameters.id);
+    if (!note)
+      throw new HTTPError(
+        404,
+        `Note with id: ${event.pathParameters.id} was not found`
+      );
+    if (input.title) note.title = input.title;
+    if (input.description) note.description = input.description;
+    await note.save();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(note)
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { "Content-Type": "text/plain" },
+      body: err.message || "Could not update the Note."
+    };
+  }
+};
+
+module.exports.destroy = async event => {
+  try {
+    const { Note } = await connectToDatabase();
+    const note = await Note.findById(event.pathParameters.id);
+    if (!note)
+      throw new HTTPError(
+        404,
+        `Note with id: ${event.pathParameters.id} was not found`
+      );
+    await note.destroy();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(note)
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { "Content-Type": "text/plain" },
+      body: err.message || "Could destroy fetch the Note."
+    };
+  }
+};
