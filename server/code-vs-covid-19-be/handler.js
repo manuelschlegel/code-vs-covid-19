9265f32fff2bf4447dde60ad863d555e278e3cc1
Manuel Schlegel <manuel.schlegel@netlight.com>
2020-03-30 00:42:10 +0200

    Fix.

--- server/code-vs-covid-19-be/handler.js
+++ server/code-vs-covid-19-be/handler.js
@@ -122,7 +122,7 @@ module.exports.getUser = async event => {
     }
 
     // Get user scores and ranks.
-    const rankedUsers = await sequelize.query(
+    const [rankedUsers, metadata] = await sequelize.query(
       "SELECT * FROM code_vs_covid_19_db.users ORDER BY (lastScore - dailyConnections - POW(dailyConnections, 1.2)) DESC"
     );
 
